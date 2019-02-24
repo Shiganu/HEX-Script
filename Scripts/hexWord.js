89235129885438a1c6bbe2dafec1f6ce80c547fe
Dareka826 <minecraftredstonemaniak@gmail.com>
2019-02-24 15:56:22 +0100

    Move functionality into a class

---
+++ Scripts/hexWord.js
@@ -0,0 +1,55 @@
+
+function HexWord(x, y, r, tr, colors)
+{
+    this.pos = [x, y];
+    this.r = r;
+    this.triR = tr;
+    this.colors = colors;
+    this.color = this.colors.length-1;
+
+    this.triangles = [];
+
+    for(let i = 0; i < 3; i++)
+    {
+        let aa = -90 + i*120;
+
+        for(let a = -60; a < 300; a += 60)
+            this.triangles.push(new Triangle(
+                this.pos[0]  + Math.sqrt(3)*this.r * cos(aa) + this.r * cos(a),
+                this.pos[1] + Math.sqrt(3)*this.r * sin(aa) + this.r * sin(a),
+                this.triR, 180-a, this.colors
+            ));
+
+        this.triangles.push(new Triangle(
+            this.pos[0]  + Math.sqrt(3)*this.r * cos(-30+i*120) + this.r * cos(-60+i*120),
+            this.pos[1] + Math.sqrt(3)*this.r * sin(-30+i*120) + this.r * sin(-60+i*120),
+            this.triR, 180-60, this.colors
+        ));
+
+        this.triangles.push(new Triangle(
+            this.pos[0]  + Math.sqrt(3)*this.r * cos(-30+i*120) + this.r * cos(0+i*120),
+            this.pos[1] + Math.sqrt(3)*this.r * sin(-30+i*120) + this.r * sin(0+i*120),
+            this.triR, 180-0, this.colors
+        ));
+    }
+
+    this.draw = function()
+    {
+        for(let t of this.triangles)
+            t.draw();
+    };
+
+    this.click = function(x, y)
+    {
+        for(let t of this.triangles)
+            t.click(x, y);
+    };
+
+    this.getWord = function()
+    {
+        let word = [];
+        for(let t of this.triangles)
+            word.push(t.color);
+        return word;
+    };
+}
--- Scripts/main.js
+++ Scripts/main.js
@@ -1,52 +1,16 @@
 
-let triangles = [];
 let r = 100;
 let triR = 80;
 let colors = ["#00ff00", "#ff0000", "#ffffff"];
-let word = [];
 
-for(let i = 0; i < 3; i++)
-{
-	let aa = -90 + i*120;
-
-	for(let a = -60; a < 300; a += 60)
-		triangles.push(new Triangle(
-			width/2  + Math.sqrt(3)*r * cos(aa) + r * cos(a),
-			height/2 + Math.sqrt(3)*r * sin(aa) + r * sin(a),
-			triR, 180-a, colors
-		));
-
-	triangles.push(new Triangle(
-		width/2  + Math.sqrt(3)*r * cos(-30+i*120) + r * cos(-60+i*120),
-		height/2 + Math.sqrt(3)*r * sin(-30+i*120) + r * sin(-60+i*120),
-		triR, 180-60, colors
-	));
-
-	triangles.push(new Triangle(
-		width/2  + Math.sqrt(3)*r * cos(-30+i*120) + r * cos(0+i*120),
-		height/2 + Math.sqrt(3)*r * sin(-30+i*120) + r * sin(0+i*120),
-		triR, 180-0, colors
-	));
-}
-
-for(let t of triangles)
-	word.push(t.color);
-
-/*for(let i = 0; i < triangles.length; i++)
-{
-	triangles[i].colors = ["#"+Math.round(225/24*i).toString(16)+Math.round(225/24*i).toString(16)+Math.round(225/24*i).toString(16)];
-	triangles[i].color = 0;
-	if(triangles[i].colors[0] == "#999") triangles[i].colors[0] = "#090909";
-	console.log(triangles[i].colors[0]);
-}*/
+let hexWord = new HexWord(width/2, height/2, r, triR, colors);
 
 function main()
 {
 	ctx.fillStyle = "#222222";
 	ctx.fillRect(0, 0, width, height);
 
-	for(let t of triangles)
-		t.draw();
+	hexWord.draw();
 
 	requestAnimationFrame(main);
 }
@@ -64,10 +28,5 @@ document.addEventListener("click", function(){
 	let mouseX = arguments[0].clientX;
 	let mouseY = arguments[0].clientY;
 
-	for(let t of triangles)
-		t.click(mouseX, mouseY);
-
-	word = [];
-	for(let t of triangles)
-		word.push(t.color);
+	hexWord.click(mouseX, mouseY);
 });
